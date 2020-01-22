8da9a57ff0505f8ea7ad8c43e98c77f97b628607
John Kindem <461425614@qq.com>
2020-01-22 21:26:02 +0800

    add some about plugin loader

--- runtime/src/main/java/pers/kindem/zed/runtime/loader/PluginLoader.java
+++ runtime/src/main/java/pers/kindem/zed/runtime/loader/PluginLoader.java
@@ -1,17 +1,28 @@
 package pers.kindem.zed.runtime.loader;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
+import pers.kindem.zed.runtime.loader.proxy.PluginApplication;
 import pers.kindem.zed.runtime.loader.proxy.PluginClassLoader;
+import pers.kindem.zed.runtime.loader.proxy.PluginResources;
 import pers.kindem.zed.runtime.utils.LogUtil;
 
 public class PluginLoader {
     private static final String TAG = PluginLoader.class.getSimpleName();
-    
-    public static Context loadComponent(PluginClassLoader pluginClassLoader, String component, Context hostContext) {
+
+    private static Map<String, PluginClassLoader> pluginClassLoaderMap = new HashMap<>();
+    private static Map<String, PluginResources> pluginResourcesMap = new HashMap<>();
+    private static Map<String, PluginApplication> pluginApplicationMap = new HashMap<>();
+
+    public static Context loadPluginComponent(PluginClassLoader pluginClassLoader, String component, Context hostContext) {
         Class<?> pluginComponentClass = null;
         try {
             pluginComponentClass = pluginClassLoader.loadClass(component);
@@ -43,5 +54,69 @@ public class PluginLoader {
         }
 
         return pluginComponent;
+    }
+
+    public static PluginClassLoader loadPluginClassLoader(
+        String pluginName, String apkPath, String optDir, String libDir, ClassLoader hostClassLoader
+    ) {
+        synchronized (PluginLoader.class) {
+            if (pluginClassLoaderMap.containsKey(pluginName)) {
+                return pluginClassLoaderMap.get(pluginName);
+            } else {
+                PluginClassLoader pluginClassLoader = new PluginClassLoader(apkPath, optDir, libDir, hostClassLoader);
+                pluginClassLoaderMap.put(pluginName, pluginClassLoader);
+                return pluginClassLoader;
+            }
+        }
+    }
+
+    public static PluginApplication loadPluginApplication(
+        String pluginName, PluginClassLoader pluginClassLoader, String applicationName, Context hostContext
+    ) {
+        synchronized (PluginLoader.class) {
+            if (pluginApplicationMap.containsKey(pluginName)) {
+                return pluginApplicationMap.get(pluginName);
+            } else {
+                PluginApplication pluginApplication = (PluginApplication) loadPluginComponent(
+                    pluginClassLoader, applicationName, hostContext);
+                pluginApplicationMap.put(pluginName, pluginApplication);
+                return pluginApplication;
+            }
+        }
+    }
+
+    public static PluginResources loadPluginResources(
+        String pluginName, String apkPath, Context hostContext
+    ) {
+        synchronized (PluginLoader.class) {
+            if (pluginResourcesMap.containsKey(pluginName)) {
+                return pluginResourcesMap.get(pluginName);
+            } else {
+                PackageManager packageManager = hostContext.getPackageManager();
+                ApplicationInfo applicationInfo = null;
+                try {
+                    applicationInfo = packageManager.getApplicationInfo(hostContext.getPackageName(), 0);
+                } catch (PackageManager.NameNotFoundException e) {
+                    LogUtil.e(TAG, "failed to get application info of host context");
+                }
+                if (applicationInfo == null) {
+                    return null;
+                }
+                applicationInfo.sourceDir = apkPath;
+                applicationInfo.publicSourceDir = apkPath;
+                Resources injectResources = null;
+                try {
+                    injectResources = packageManager.getResourcesForApplication(applicationInfo);
+                } catch (PackageManager.NameNotFoundException e) {
+                    LogUtil.e(TAG, "failed to create inject resources");
+                }
+                if (injectResources == null) {
+                    return null;
+                }
+                PluginResources pluginResources = new PluginResources(hostContext.getResources(), injectResources);
+                pluginResourcesMap.put(pluginName, pluginResources);
+                return pluginResources;
+            }
+        }
     }
 }
--- runtime/src/main/java/pers/kindem/zed/runtime/loader/proxy/PluginResources.java
+++ runtime/src/main/java/pers/kindem/zed/runtime/loader/proxy/PluginResources.java
@@ -4,8 +4,9 @@ import android.content.res.Resources;
 
 public class PluginResources extends Resources {
     private Resources hostResources;
+    private Resources injectResources;
 
-    public PluginResources(Resources hostResources) {
+    public PluginResources(Resources hostResources, Resources injectResources) {
         super(hostResources.getAssets(), hostResources.getDisplayMetrics(), hostResources.getConfiguration());
         this.hostResources = hostResources;
     }
