01ce0974fcf18f5c488509d4c8b8ac089382f045
John Kindem <461425614@qq.com>
2020-01-28 00:33:00 +0800

    add some code about resources

--- runtime/src/main/java/pers/kindem/zed/runtime/loader/proxy/PluginResources.java
+++ runtime/src/main/java/pers/kindem/zed/runtime/loader/proxy/PluginResources.java
@@ -1,13 +1,56 @@
 package pers.kindem.zed.runtime.loader.proxy;
 
+import android.annotation.TargetApi;
 import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.os.Build;
 
 public class PluginResources extends Resources {
+    private static final String TAG = PluginResources.class.getSimpleName();
+
     private Resources hostResources;
     private Resources injectResources;
 
     public PluginResources(Resources hostResources, Resources injectResources) {
         super(hostResources.getAssets(), hostResources.getDisplayMetrics(), hostResources.getConfiguration());
         this.hostResources = hostResources;
+        this.injectResources = injectResources;
+    }
+
+    @Override
+    public XmlResourceParser getLayout(int id) throws NotFoundException {
+        try {
+            return injectResources.getLayout(id);
+        } catch (NotFoundException e) {
+            return hostResources.getLayout(id);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public int getColor(int id, Theme theme) throws NotFoundException {
+        try {
+            return injectResources.getColor(id, theme);
+        } catch (NotFoundException e) {
+            return hostResources.getColor(id, theme);
+        }
+    }
+
+    @Override
+    public CharSequence getText(int id) throws NotFoundException {
+        try {
+            return injectResources.getText(id);
+        } catch (NotFoundException e) {
+            return hostResources.getText(id);
+        }
+    }
+
+    @Override
+    public CharSequence getText(int id, CharSequence def) {
+        try {
+            return injectResources.getText(id, def);
+        } catch (NotFoundException e) {
+            return hostResources.getText(id);
+        }
     }
 }
