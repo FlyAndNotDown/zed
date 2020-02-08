d33c394fdf1cc0d9cc69009f6ab45c7273ce6be6
John Kindem <461425614@qq.com>
2020-02-09 02:21:30 +0800

    add some code about spider module

--- spider/src/spider.ts
+++ spider/src/spider.ts
@@ -1,7 +1,8 @@
 import { config } from "./config";
 import * as Cheerio from 'cheerio';
-import {Log} from "./log";
-import {default as Axios} from "axios";
+import { Log } from "./log";
+import { default as Axios } from "axios";
+import * as FileSystem from 'fs';
 
 export interface API {
 
@@ -26,17 +27,23 @@ export class Spider {
     }
 
     private static parseApisObjectToText(apis: API[]): string {
-        return null;
+        const result: string = '';
+        apis.forEach(api => {});
+        return result;
     }
 
-    private static save(content: string): void {
-        return null;
+    private static save(file: string, content: string): void {
+        FileSystem.writeFileSync(file, content);
     }
 
     public static async fetchActivityApis(): Promise<void> {
-        const source = await Spider.getSourceFromUrl(config.url.activity);
-        const splitTemp = source.split('<!-- ========= METHOD DETAIL ======== -->');
-        const publicMethodSource = splitTemp[1];
-        const protectedMethodSource = splitTemp[2].split('<!-- ========= END OF CLASS DATA ========= -->')[0];
+        const source: string = await Spider.getSourceFromUrl(config.url.activity);
+        const splitTemp: string[] = source.split('<!-- ========= METHOD DETAIL ======== -->');
+        const publicMethodSource: string = splitTemp[1];
+        const protectedMethodSource: string = splitTemp[2].split('<!-- ========= END OF CLASS DATA ========= -->')[0];
+        const publicApis: API[] = Spider.getApis(publicMethodSource);
+        const protectedApis: API[] = Spider.getApis(protectedMethodSource);
+        Spider.save('../output/activity/public.txt', Spider.parseApisObjectToText(publicApis));
+        Spider.save('../output/activity/protected', Spider.parseApisObjectToText(protectedApis));
     }
 }
