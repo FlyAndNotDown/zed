91efa1c2d4a75896e6620a8a9b2579e5d32e39d4
John Kindem <461425614@qq.com>
2020-02-13 01:03:34 +0800

    some refactor about spider module

--- spider/src/spider.ts
+++ spider/src/spider.ts
@@ -5,24 +5,32 @@ import { NetworkUtil } from "./utils/network";
 import { PageResolutionUtil } from "./utils/page-resolution";
 import { FileUtil } from "./utils/file";
 import { API, ApiExcelSheet } from "./type/api";
+import { Constant } from "./utils/Constant";
 
 export class Spider {
-    public static async fetchActivityApis(): Promise<void> {
-        const source: string = await NetworkUtil.getSourceFromUrl(config.url.activity);
-        const splitTemp: string[] = source.split('<!-- ========= METHOD DETAIL ======== -->');
+    private static async fetchApis(taskName: string, url: string, savePath: string): Promise<void> {
+        const source: string = await NetworkUtil.getSourceFromUrl(url);
+        const splitTemp: string[] = source.split(Constant.PUBLIC_METHOD_TAG);
         const publicMethodSource: string = splitTemp[1];
-        const protectedMethodSource: string = splitTemp[2].split('<!-- ========= END OF CLASS DATA ========= -->')[0];
+        const protectedMethodSource: string = splitTemp[2].split(Constant.PROTECTED_METHOD_TAG)[0];
 
-        LogUtil.log('info', 'starting fetch activity public apis');
+        LogUtil.log('info', `starting fetch ${taskName} public apis`);
         const publicApis: API[] = PageResolutionUtil.getApis(publicMethodSource);
         const publicApiExcelSheet: ApiExcelSheet = FileUtil.parseApisToExcelSheet(publicApis, 'public');
 
-        LogUtil.log('info', 'starting fetch activity protected apis');
+        LogUtil.log('info', `starting fetch ${taskName} protected apis`);
         const protectedApis: API[] = PageResolutionUtil.getApis(protectedMethodSource);
         const protectedApiExcelSheet: ApiExcelSheet = FileUtil.parseApisToExcelSheet(protectedApis, 'protected');
 
-        const savePath: string = Path.resolve(config.save, 'activity', 'apis.xlsx');
         LogUtil.log('info', `saved apis to ${savePath}`);
         FileUtil.save(savePath, [publicApiExcelSheet, protectedApiExcelSheet]);
+    }
+
+    public static async fetchActivityApis(): Promise<void> {
+        await Spider.fetchApis(
+            'activity',
+            config.url.activity,
+            Path.resolve(config.save, 'activity', 'apis.xlsx')
+        );
     }
 }
---
+++ spider/src/utils/Constant.ts
@@ -0,0 +1,4 @@
+export class Constant {
+    public static PUBLIC_METHOD_TAG: string = '<!-- ========= METHOD DETAIL ======== -->';
+    public static PROTECTED_METHOD_TAG: string = '<!-- ========= END OF CLASS DATA ========= -->';
+}
